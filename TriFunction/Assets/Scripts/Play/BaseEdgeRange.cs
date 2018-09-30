659ae49e3b28257c8e0f7fcebb9a584caea6373a
JeonChangMin <wisechang1@naver.com>
2018-09-30 11:48:14 +0900

    삼각형 모서리, 꼭짓점 이펙트, 범위지정함. 단, 아직 Script는 생성만하고 작성못함

---
+++ TriFunction/Assets/Scripts/Play/BaseEdgeRange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEdgeRange : MonoBehaviour {
+    public GameObject BaseEdge_Range;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TriFunction/Assets/Scripts/Play/CoAngleRange.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoAngleRange : MonoBehaviour {
+    public GameObject CoAngle_Range;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
