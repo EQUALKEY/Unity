75475935bdfa76d60d04975b58083e10d4cb8bdd
Andy <andy@quebon.com>
2018-11-25 12:39:35 +0900

    rank api

--- TriFunction/Assets/Scripts/Play/RankManager.cs
+++ TriFunction/Assets/Scripts/Play/RankManager.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 using System.Runtime.InteropServices;
 using System;
 
 public class RankManager : MonoBehaviour {
+
+    public const string gameName = "TriFunc";
 
     public GameObject EC;
     
@@ -21,7 +24,8 @@ public class RankManager : MonoBehaviour {
     
     // UserData 저장용 구조체
     struct UserData {
-        public string userid;
+        public string host;
+        public string userId;
         public string nickname;
         public string token;
     }
@@ -38,21 +42,48 @@ public class RankManager : MonoBehaviour {
         user = JsonUtility.FromJson<UserData>(UserJsonData);
     }
 
+    struct Badge
+    {
+        public int level;
+    }
+
+    struct User
+    {
+        public string userId;
+        public string nickname;
+        public Dictionary<string, Badge> badges;
+    }
+
+    struct Ranking
+    {
+        public RankData my;
+        public List<RankData> ranking;
+    }
+
     // RankData 저장할 구조체
     struct RankData {
-        public int Rank;        // 등수
-        public int Score;       // 점수
-        public int Time;        // 시간
-        public string Nickname; // 닉네임
-        public int Level;       // 레벨 (깨봉홈페이지 레벨)
+        public User user;
+        public int rank;        // 등수
+        public int score;       // 점수
+        public int time;        // 시간
+        public string nickname; // 닉네임
+        public int level;       // 레벨 (깨봉홈페이지 레벨)
     }
 
     // 상위 5등과 자신의 RankData 저장할 구조체
     RankData[] Top5 = new RankData[5];
+
     RankData MyRank;
 
     // DB에 정보 전송, 점수-시간-userid 를 보낸다
     public void PushRankInfo(int score, int time) {
+        if (string.IsNullOrEmpty(user.token))
+        {
+            //not authorized
+            return;
+        }
+
+        PutRanking(user.token, score, time);
 
         // 점수는 (int) score, 시간은 (int) time, userid는 (string) user.userid 에 저장되어있다
         // 이 값들을 DB로 보내 저장한다.
@@ -62,6 +93,11 @@ public class RankManager : MonoBehaviour {
     // token은 (string) user.token에 저장되어 있다.
     // 받아오는 데이터는 각각의 등수, 점수, 시간, 닉네임, 레벨
     private void GetRankInfo() {
+        if (string.IsNullOrEmpty(user.token)) {
+            //not authorized
+            return;
+        }
+        GetRanking(user.token);
         // RankData MyRank, Top5[5]; 에서
         // MyRank에는 플레이어의 정보 저장
         // Top5[5]에는 상위 5명 정보 저장
@@ -131,5 +167,68 @@ public class RankManager : MonoBehaviour {
         RankWindow.SetActive(false);
         RankImage.SetActive(false);
         RankCloseButton.SetActive(false);
+    }
+
+    private IEnumerator GetRanking(string token)
+    {
+        string url = user.host + "/user/v1/games/" + gameName;
+
+        using (UnityWebRequest w = UnityWebRequest.Get(url))
+        {
+            w.SetRequestHeader("Authorization", "Bearer " + token);
+            yield return w.SendWebRequest();
+
+            if (w.isHttpError || w.isNetworkError) {
+                //TODO handle error
+            }
+            else{
+                //success
+                Ranking r = JsonUtility.FromJson<Ranking>(w.downloadHandler.text);
+
+                MyRank = r.my;
+                MyRank.nickname = r.my.user.nickname;
+                if (r.my.user.badges.ContainsKey("winner")) {
+                    MyRank.level = r.my.user.badges["winner"].level;
+                }
+
+                int size = Math.Min(r.ranking.Count, 5);
+                int i = 0;
+                for (i = 0; i < size; i++) {
+                    Top5[i] = r.ranking[i];
+                    Top5[i].nickname = r.ranking[i].user.nickname;
+                    if (r.my.user.badges.ContainsKey("winner"))
+                    {
+                        Top5[i].level = r.ranking[i].user.badges["winner"].level;
+                    }
+                }
+                if (i < 5) {
+                    for (int j = i; j < 5; j++) {
+                        //TODO don't show empty data
+                        Top5[j] = new RankData();
+                    }
+                }
+            }
+        }
+    }
+
+    private IEnumerator PutRanking(string token, int score, int time)
+    {
+        string url = user.host + "/user/v1/games/" + gameName + "/users/" + user.userId;
+        string data = "{\"score\":" + score + ",\"time\":" + time + "}";
+
+        using (UnityWebRequest w = UnityWebRequest.Put(url, data))
+        {
+            w.SetRequestHeader("Authorization", "Bearer " + token);
+            yield return w.SendWebRequest();
+
+            if (w.isHttpError || w.isNetworkError)
+            {
+                //TODO handle error
+            }
+            else
+            {
+                //sucess
+            }
+        }
     }
 }
